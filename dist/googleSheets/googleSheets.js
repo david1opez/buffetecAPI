c6ad69efbe896c1f1b4979502483b4af290dbdec
David Lopez <72664248+david1opez@users.noreply.github.com>
2024-10-08 14:36:01 -0600

    Refactor Google Sheets authentication and credentials handling

--- dist/googleSheets/googleSheets.js
+++ dist/googleSheets/googleSheets.js
@@ -14,7 +14,7 @@ async function getGoogleSheetClient() {
     const serviceAccountCredentials = {
         "type": "service_account",
         "project_id": process.env.PROJECT_ID,
-        "private_key_id": process.env.PRIVATE_KEY_ID,
+        "private_key_id": process.env.PRIVATE_KEY_ID?.replaceAll("\\n", "\n").replaceAll('"', ''),
         "private_key": process.env.PRIVATE_KEY,
         "client_email": process.env.CLIENT_EMAIL,
         "client_id": process.env.CLIENT_ID,
--- src/googleSheets/googleSheets.ts
+++ src/googleSheets/googleSheets.ts
@@ -7,7 +7,7 @@ export async function getGoogleSheetClient(): Promise<sheets_v4.Sheets> {
   const serviceAccountCredentials = {
     "type": "service_account",
     "project_id": process.env.PROJECT_ID,
-    "private_key_id": process.env.PRIVATE_KEY_ID,
+    "private_key_id": process.env.PRIVATE_KEY_ID?.replaceAll("\\n", "\n").replaceAll('"', ''),
     "private_key": process.env.PRIVATE_KEY,
     "client_email": process.env.CLIENT_EMAIL,
     "client_id": process.env.CLIENT_ID,

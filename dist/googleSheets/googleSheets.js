f7243163cbcf3137d0d4959b95f0abed705ccc59
David Lopez <72664248+david1opez@users.noreply.github.com>
2024-10-08 13:44:47 -0600

    Refactor Google Sheets authentication and credentials handling

--- dist/googleSheets/googleSheets.js
+++ dist/googleSheets/googleSheets.js
@@ -1,4 +1,27 @@
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -9,9 +32,28 @@ exports.writeSheet = writeSheet;
 exports.updateSheet = updateSheet;
 const googleapis_1 = require("googleapis");
 const dotenv_1 = __importDefault(require("dotenv"));
+const fs = __importStar(require("fs"));
+const path = __importStar(require("path"));
 dotenv_1.default.config();
-const serviceAccountKeyFile = process.cwd() + '/src/service.json';
+const serviceAccountKeyFile = path.join(process.cwd(), '/src/service.json');
 async function getGoogleSheetClient() {
+    // Service account credentials
+    const serviceAccountCredentials = {
+        "type": "service_account",
+        "project_id": process.env.PROJECT_ID,
+        "private_key_id": process.env.PRIVATE_KEY_ID,
+        "private_key": process.env.PRIVATE_KEY,
+        "client_email": process.env.CLIENT_EMAIL,
+        "client_id": process.env.CLIENT_ID,
+        "auth_uri": process.env.AUTH_URI,
+        "token_uri": process.env.TOKEN_URI,
+        "auth_provider_x509_cert_url": process.env.AUTH_PROVIDER_X509_CERT_URL,
+        "client_x509_cert_url": process.env.CLIENT_X509_CERT_URL,
+        "universe_domain": process.env.UNIVERSE_DOMAIN
+    };
+    if (!fs.existsSync(serviceAccountKeyFile)) {
+        fs.writeFileSync(serviceAccountKeyFile, JSON.stringify(serviceAccountCredentials, null, 2));
+    }
     const auth = new googleapis_1.google.auth.GoogleAuth({
         keyFilename: serviceAccountKeyFile,
         scopes: ['https://www.googleapis.com/auth/spreadsheets'],
--- src/googleSheets/googleSheets.ts
+++ src/googleSheets/googleSheets.ts
@@ -1,11 +1,32 @@
 import { google, sheets_v4 } from 'googleapis';
 import dotenv from 'dotenv';
+import * as fs from 'fs';
+import * as path from 'path';
 
 dotenv.config();
 
-const serviceAccountKeyFile = process.cwd() + '/src/service.json';
+const serviceAccountKeyFile = path.join(process.cwd(), '/src/service.json');
 
 export async function getGoogleSheetClient(): Promise<sheets_v4.Sheets> {
+  // Service account credentials
+  const serviceAccountCredentials = {
+    "type": "service_account",
+    "project_id": process.env.PROJECT_ID,
+    "private_key_id": process.env.PRIVATE_KEY_ID,
+    "private_key": process.env.PRIVATE_KEY,
+    "client_email": process.env.CLIENT_EMAIL,
+    "client_id": process.env.CLIENT_ID,
+    "auth_uri": process.env.AUTH_URI,
+    "token_uri": process.env.TOKEN_URI,
+    "auth_provider_x509_cert_url": process.env.AUTH_PROVIDER_X509_CERT_URL,
+    "client_x509_cert_url": process.env.CLIENT_X509_CERT_URL,
+    "universe_domain": process.env.UNIVERSE_DOMAIN
+  };
+
+  if (!fs.existsSync(serviceAccountKeyFile)) {
+    fs.writeFileSync(serviceAccountKeyFile, JSON.stringify(serviceAccountCredentials, null, 2));
+  }
+
   const auth = new google.auth.GoogleAuth({
     keyFilename: serviceAccountKeyFile,
     scopes: ['https://www.googleapis.com/auth/spreadsheets'],

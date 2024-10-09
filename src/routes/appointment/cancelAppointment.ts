57be921c47090cc37902eb1c23d2745dd4c1bb2b
ramiro-uziel <ramirouziel15@gmail.com>
2024-10-09 13:35:08 -0600

    Funcion para endpoint, cancel appointment.

---
+++ src/routes/appointment/cancelAppointment.ts
@@ -0,0 +1,36 @@
+// cancelAppointment.ts
+import { collection } from "../../mongo/mongo";
+import { Request, Response } from "express";
+import { Appointment } from "./appointmentTypes";
+import { ObjectId } from "mongodb";
+
+export default async function CancelAppointment(req: Request, res: Response) {
+  try {
+    const { _id } = req.body;
+
+    if (!_id || typeof _id !== "string") {
+      return res.status(400).json({ error: "Invalid appointment ID provided" });
+    }
+
+    const updateFields: Partial<Appointment> = {
+      updatedAt: new Date(),
+    };
+
+    const updateResult = await collection("citas").findOneAndUpdate(
+      { _id: new ObjectId(_id) },
+      { $set: { ...updateFields, estatus: "cancelada" } },
+      { returnDocument: "after" }
+    );
+
+    if (!updateResult) {
+      return res.status(404).json({ error: "Appointment not found" });
+    }
+
+    res.status(200).json(updateResult);
+  } catch (error) {
+    console.error("Error updating appointment:", error);
+    res
+      .status(500)
+      .json({ error: "Internal server error", details: error.message });
+  }
+}
